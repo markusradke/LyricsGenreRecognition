6e95367ed501225bd05acbad780faa24f80fb7ea
= <=>
2026-02-09 09:39:23 +0100

    make model reports for more informed ngrams

--- src/helpers/generate_report.py
+++ src/helpers/generate_report.py
@@ -49,3 +49,37 @@ def _add_timestamp_footer(html_path: Path, timestamp: str) -> None:
 
     with open(html_path, "w", encoding="utf-8") as f:
         f.write(content)
+
+
+def report_model_evaluation(experiment):
+    "Print a comprehensive report of the model evaluation results for a given experiment."
+    experiment.show_train_test_genrefreq_comparison()
+    print("Random Baseline Evaluation:")
+    print("-" * 40)
+    experiment.show_random_baseline_evaluation()
+
+    print("\n\n")
+    print("Tuning History:")
+    print("-" * 40)
+    experiment.show_tuning_history()
+
+    print("\n\n")
+    print("Model evaluation on Holdout Set:")
+    print("-" * 40)
+    experiment.show_model_evaluation()
+
+    print("\n\n")
+    print("Top Coefficients per Genre:")
+    print("-" * 40)
+    _show_top_coefficients_per_genre(experiment.model_coefficients, top_n=10)
+
+
+def _show_top_coefficients_per_genre(coeffs, top_n=10):
+    """Show the most important features for each genre based on the logistic regressions' model coefficients."""
+    genres = coeffs.columns
+    for genre in genres:
+        print(f"Top {top_n} coefficients for genre: {genre.upper()}")
+        top_coeffs = coeffs[genre].abs().sort_values(ascending=False).head(top_n)
+        for feature, value in top_coeffs.items():
+            print(f"{feature} ({coeffs.at[feature, genre]:.3f})")
+        print("\n")
