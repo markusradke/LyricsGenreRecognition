5f3f7f0406f2c8cccb6cff80e438cc44175c8710
= <=>
2026-03-07 15:33:00 +0100

    re-estimate performance ceiling for english corpus only

--- analysis/05_estimate_interrater_agreement.py
+++ analysis/05_estimate_interrater_agreement.py
@@ -3,6 +3,15 @@ from helpers.interrater_agreement import monte_carlo_interrater_agreement_f1
 from helpers.generate_report import generate_report
 
 genre_counts = pd.read_csv("data-raw/poptrag_lyrics_genre_counts.csv")
+english = pd.read_csv(
+    "data/poptrag_lyrics_genres_corpus_filtered_english_lemmatized.csv"
+)
+id_in_english = set(english["track.s.id"])
+genre_counts = genre_counts[genre_counts["track.s.id"].isin(id_in_english)]
+genre_counts = genre_counts.loc[
+    :, ~genre_counts.columns.str.contains("classical|schlager", case=False)
+]
+
 
 N_MC = 10000  # Number of Monte Carlo simulations
 SEED = 42
@@ -14,11 +23,5 @@ for granularity in GRANULARITIES:
         genre_counts, granularity=granularity, n_mc=N_MC, seed=SEED
     )
     ira_result.to_csv(f"models/interrater_agreement/ira_cat{granularity}.csv")
-
-print("GENERATE REPORT...")
-generate_report(
-    notebook_path="notebooks/reporting/02_estimate_interrater_agreement.ipynb",
-    output_path="reports",
-)
 
 print("DONE.")
